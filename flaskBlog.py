cebb38fb798443ccc8f8c2340b51c4cbc22ebe95
Tony Liu <tliut@yahoo.com>
2020-10-28 11:59:02 -0500

    Initial Commit

---
+++ flaskBlog.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Tony Liu',
+        'title':    'Blog Post 1',
+        'content':  'First Post Content',
+        'date_posted':  'Oct 25, 2020'
+    },
+    {
+        'author': 'Vivian Liu',
+        'title':    'Blog Post 2',
+        'content':  'Second Post Content',
+        'date_posted':  'Oct 25, 2020'
+    }
+]
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts= posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
