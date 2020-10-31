69842e87eaf5a19fff98d1be55e9f2930b51a404
Tony Liu <tliut@yahoo.com>
2020-10-30 23:03:15 -0500

    Added Templates, Forms and User Input Functionality

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+# Use secrets module in python "secrets.token_hex(16) to generate a random secret"
+
+app.config['SECRET_KEY'] = 'af475c5067b73cd007dbc8805acfc201'
 posts = [
     {
         'author': 'Tony Liu',
@@ -23,6 +27,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template('login.html', title='login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
