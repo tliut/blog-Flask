0eea2fe330ede61726838b21f8524eb92b3a01a8
Tony Liu <tliut@yahoo.com>
2020-10-31 04:29:51 -0500

    Implemented DB functionality using SQLAlchemy and added Instructions.txt

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,10 +1,40 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 # Use secrets module in python "secrets.token_hex(16) to generate a random secret"
-
 app.config['SECRET_KEY'] = 'af475c5067b73cd007dbc8805acfc201'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:password@tl-test-mysql.cs3minrzf996.us-east-2.rds.amazonaws.com/flaskBlogDB'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+    
 posts = [
     {
         'author': 'Tony Liu',
